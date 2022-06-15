c419f52550236ca7473739f69527778ffdad8da1
jered <me@jered.dev>
2022-06-15 16:42:05 -0600

    Create TDD class.
    Created the DeskBookingRequestProcessorTests class. Began Arrange/Act/Assert in new [Fact] class to fulfill requirements. Created classes and methods from Test class.

---
+++ DeskBooker.Core.Tests/Processor/DeskBookingRequestProcessor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DeskBooker.Core.Processor
+{
+    internal class DeskBookingRequestProcessor
+    {
+        public DeskBookingRequestProcessor()
+        {
+        }
+
+        internal DeskBookingResult BookDesk(DeskBookingRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DeskBooker.Core.Tests/Processor/DeskBookingRequestProcessorTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DeskBooker.Core.Processor
+{
+    public class DeskBookingRequestProcessorTests
+    {
+        [Fact]
+        public void ShouldReturnDeskBookingResultWithRequestValues()
+        {
+            // Arrange
+            var request = new DeskBookingRequest
+            {
+                FirstName = "Jered",
+                LastName = "Sanchez",
+                Email = "valak@aol.com",
+                Date = new DateTime(2022, 6, 15)
+            };
+
+            var processor = new DeskBookingRequestProcessor();
+            
+            // Act
+            DeskBookingResult result = processor.BookDesk(request);
+
+            // Assert
+            Assert.NotNull(result);
+            
+            Assert.Equal(request.FirstName, result.FirstName);
+            Assert.Equal(request.LastName, result.LastName);
+            Assert.Equal(request.Email, result.Email);
+            Assert.Equal(request.Date, result.Date);
+        }
+    }
+}
